88560907f59ee93ab11aa5e218261ca123385d1f
edilson258 <dev.258.edilson@gmail.com>
2025-05-14 07:46:57 +0200

    feat: impl file size parser

--- src/internal/parser.ts
+++ src/internal/parser.ts
@@ -1,20 +1,35 @@
 import { DefaultFileSizeInBytes } from "./consts"
 
-type SizeUnit = "B" | "K" | "M" | "G"
+type SizeSuffix = "B" | "K" | "M" | "G"
 
-const SizeUnitMap = {
+const SizeSuffixMap: Record<SizeSuffix, number> = {
   "B": 1,
   "K": 1 * 1024,
   "M": 1 * 1024 * 1024,
   "G": 1 * 1024 * 1024 * 1024,
 }
 
-export function parseFileSizeToBytes(size: string): number {
-  size = size.trim()
-  if (size.length == 0) return DefaultFileSizeInBytes
+export function parseFileSizeToBytes(input: string): number {
+  input = input.trim()
+  if (input.length == 0) return DefaultFileSizeInBytes
 
   // expect one of: B, K, M, G
-  const sizeUnitDigit = size[size.length - 1]!.toUpperCase()
+  const suffixChar = input[input.length - 1]!.toUpperCase()
+  const [suffix, hasValidSuffix] = suffixChar in SizeSuffixMap
+    ? [suffixChar as SizeSuffix, true]
+    : ["B" as SizeSuffix, false]
 
-  return 0
+  if (!hasValidSuffix) {
+    console.log("Invalid size suffix, defaulting to B (bytes)")
+  }
+
+  const size = hasValidSuffix ? parseFloat(input.slice(0, -1)) : parseFloat(input);
+
+  if (isNaN(size)) {
+    console.log("Invalid size format, defaulting to 1K")
+    return DefaultFileSizeInBytes;
+  }
+
+  const sizeSuffixMultiplier = SizeSuffixMap[suffix];
+  return (size * sizeSuffixMultiplier);
 }
